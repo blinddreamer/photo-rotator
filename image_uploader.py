96f82ae9b2e0315adce584293f21b5525bbf5d0a
Nikolay Apostolov <blinddreamer.huku.rocks>
2023-05-25 02:45:36 +0300

    change every 15 m

--- image_uploader.py
+++ image_uploader.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 from flask import Flask, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
@@ -7,13 +8,30 @@ UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png'}
 IMAGE_WIDTH = 600
 IMAGE_HEIGHT = 800
+PHOTO_CHANGE_INTERVAL = 15 * 60  # 15 minutes (in seconds)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+last_photo_update_time = 0
+current_photo = None
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_random_photo():
+    global last_photo_update_time, current_photo
+
+    current_time = time.time()
+    if current_photo is None or current_time - last_photo_update_time >= PHOTO_CHANGE_INTERVAL:
+        files = os.listdir(app.config['UPLOAD_FOLDER'])
+        image_files = [file for file in files if allowed_file(file)]
+        if image_files:
+            current_photo = random.choice(image_files)
+            last_photo_update_time = current_time
+
+    return current_photo
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -46,11 +64,9 @@ def uploaded_file(filename):
 
 @app.route('/random_photo')
 def random_photo():
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
-    image_files = [file for file in files if allowed_file(file)]
-    if image_files:
-        random_image = random.choice(image_files)
-        return send_from_directory(app.config['UPLOAD_FOLDER'], random_image)
+    photo = get_random_photo()
+    if photo:
+        return send_from_directory(app.config['UPLOAD_FOLDER'], photo)
     return 'No images uploaded yet.'
 
 if __name__ == '__main__':
